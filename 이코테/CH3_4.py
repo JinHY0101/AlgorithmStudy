73f767ba3785767cfbab929ee680e240e4b46daf
JinHY0101 <90594721+JinHY0101@users.noreply.github.com>
2022-03-07 14:22:47 +0900

    Add files via upload

---
+++ 이코테/CH3_4.py
@@ -0,0 +1,15 @@
+"""이코테-그리디 1이 될때까지"""
+
+n, k = map(int,input().split())
+cnt=0
+while True:
+    target = (n//k)*k
+    cnt += n - target
+    n= target
+    if n < k :
+        break
+    cnt +=1
+    n//=k
+cnt+=(n-1)
+print(cnt)
+
